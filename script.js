5cc98f53014f23576267c4c0362d3169d5ce0c11
jeo02 <juan.ospina@stonybrook.edu>
2021-02-12 22:59:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ else
     info = JSON.parse(localStorage.getItem("info"));
 
 function updateArray(){
-    var curr = "<p>" + document.getElementById("first").value + "</p>" + "<p>" + document.getElementById("last").value + "</p>" + "<p>" + document.getElementById("dob").value + "</p>";
+    var curr = "<p>" + "<b>Name:</b> " + document.getElementById("last").value + ", " + document.getElementById("first").value + " <b>DOB:</b> " + document.getElementById("dob").value
+    + " <b>Patient ID:</b> " + document.getElementById("pid").value + "<br><b>Gender:</b> " + document.getElementById("gender").value + " <b>Ethnicity:<b> " + document.getElementById("ethnicity").value;
     info.push(curr);
     localStorage.setItem("info", JSON.stringify(info));
     alert("Succesfully submited!");
