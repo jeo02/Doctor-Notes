14900d29aaac4f1e202899a5633b25f747e40878
jeo02 <juan.ospina@stonybrook.edu>
2021-02-13 11:59:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function checkRequired()
 {
     var required = document.getElementsByClassName('required');
     for(var i = 0; i < required.length; i++){
+        alert("hi");
         if(required.item(i).value() == ""){
             alert("Please fill in all the required* fields.");
             return true;
