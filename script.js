d536b852794eecc58f7c9746a714197b753634af
jeo02 <juan.ospina@stonybrook.edu>
2021-02-12 21:36:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var arr = new Array();
+var info;
+if(localStorage.getItem("info") === null)
+    info = new Array();
+else
+    info = JSON.parse(localStorage.getItem("info"));
 
 function updateArray(){
-    
+    var curr = "<p>" + document.getElementById("first") + "</p>" + "<p>" + document.getElementById("last") + "</p>" + "<p>" + document.getElementById("dob") + "</p>";
+    info.push(curr);
+    location.reload();
+    alert("Succesfully submited!");
 }
