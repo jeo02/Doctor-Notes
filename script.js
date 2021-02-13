073f18744e0b9c773b33476d8a264ab6fc4a3c5e
jeo02 <juan.ospina@stonybrook.edu>
2021-02-12 22:07:32 -0500

    script

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var arr = new Array();
+var info;
+if(localStorage.getItem("info") === null)
+    info = new Array();
+else
+    info = JSON.parse(localStorage.getItem("info"));
 
 function updateArray(){
-    
+    var curr = "<p>" + document.getElementById("first") + "</p>" + "<p>" + document.getElementById("last") + "</p>" + "<p>" + document.getElementById("dob") + "</p>";
+    info.push(curr);
+    localStorage.setItem("info", JSON.stringify(info));
+    alert("Succesfully submited!");
+    document.getElementById("form").reset();
 }
+
 
 var navbar = document.getElementById("navBar");
 var sticky = navbar.offsetTop;
