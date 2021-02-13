d6f1f43b3281a1bc3902d60c7379b100a7ea5627
jeo02 <juan.ospina@stonybrook.edu>
2021-02-13 00:13:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ else
 
 function updateArray(){
     var curr = "<p>" + "<b>Name:</b> " + document.getElementById("last").value + ", " + document.getElementById("first").value + " <b>DOB:</b> " + document.getElementById("dob").value
-    + " <b>Patient ID:</b> " + document.getElementById("pid").value + "<br><b>Gender:</b> " + document.getElementById("gender").value + " <b>Ethnicity:<b> " + document.getElementById("ethnicity").value;
+    + " <b>Patient ID:</b> " + document.getElementById("pid").value + "<br><b>Gender:</b> " + document.getElementById("gender").value + " <b>Ethnicity:<b> " + document.getElementById("ethnicity").value 
+    + " <b>Race</b> " + document.getElementById("race").value + " <b>Height</b> " + document.getElementById("height").value + " <b>Weight</b> " + document.getElementById("weight").value
+    + "<br><b>Address:</b> " + document.getElementById("address").value + ", " + document.getElementById("city").value + ", " + document.getElementById("state").value + document.getElementById("zip").value
+    + "<br><b>Email:<b> " + document.getElementById("email").value + " <b>Cellphone:<b> " + document.getElementById("city").value + "</p>" + "<p>" + document.getElementById("notes").value + "</p>";
+    
     info.push(curr);
     localStorage.setItem("info", JSON.stringify(info));
     alert("Succesfully submited!");
