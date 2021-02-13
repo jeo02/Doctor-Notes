9199965363a5bf3268da87dfa5c478cfeedc89c5
JareBear16 <jaredtjahjadi@gmail.com>
2021-02-12 21:37:24 -0500

    Changed color scheme, tweaked navbar (made sticky and centered)

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var arr = new Array();
 function updateArray(){
     
 }
+
+var navbar = document.getElementById("navBar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
