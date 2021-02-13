245218864525cd7a8d4250653fbc52f3a6c0a034
jeo02 <juan.ospina@stonybrook.edu>
2021-02-13 12:33:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,6 @@ else
 
 function updateArray(){
 
-    var emptyInput = false;
-    var required = document.getElementsByClassName("required");
-    for(var i = 0; i < required.length; i++){
-        if(required.item(i).value() == ""){
-            alert(Hi);
-            emptyInput = true;
-        }
-    }
-    if(!emptyInput){
         var curr = "<div class = \"innerPage\"><p>" + "<b>Name:</b> " + document.getElementById("last").value + ", " + document.getElementById("first").value + " <b>DOB:</b> " + document.getElementById("dob").value
         + " <b>Patient ID:</b> " + document.getElementById("pid").value + "<br><b>Gender:</b> " + document.getElementById("gender").value + " <b>Ethnicity:</b> " + document.getElementById("ethnicity").value 
         + "<br><b>Race:</b> " + document.getElementById("race").value + " <b>Height:</b> " + document.getElementById("height").value + "ft <b>Weight:</b> " + document.getElementById("weight").value + " lbs"
@@ -25,10 +16,9 @@ function updateArray(){
         localStorage.setItem("info", JSON.stringify(info));
         alert("Succesfully submited!");
         document.getElementById("form").reset();
-    }
+    
     
 }
-
 
 function resetList(){
     localStorage.clear();
